178bafe8a4b4385da150f81e565969790687e9e9
Derek Etman <dereketman@gmail.com>
2020-01-22 17:13:16 -0800

    + Stock_prices passes all tests

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -1,10 +1,30 @@
-#!/usr/bin/python
 
 import argparse
+  # loop through arr
+    # for each element loop through the rest of the array
+    # find the difference between first element vs the next elements
+    # if the difference is greater than max_profit
+        # replace max_profit with current difference
+      # elif difference is less than min_profit
+        # replace min_profit with current difference
 
 def find_max_profit(prices):
-  pass
 
+  # variable for max_profit
+  max_profit = -999
+
+  for i in prices:
+
+    for j in prices[prices.index(i) + 1:]:
+
+      current_difference = j - i
+      print(f"i({i}), j({j}), current difference({current_difference})")
+
+      if max_profit < current_difference:
+        max_profit = current_difference
+        print(f"max Profit ({max_profit})")
+
+  return max_profit
 
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
